1ca33b26fbb0b0246edddb746486adfa844ef95b
JJ <26510108+juggeroid@users.noreply.github.com>
2025-12-01 23:44:32 +0300

    Update 2025-1.cpp

--- 2025-1.cpp
+++ 2025-1.cpp
@@ -4,28 +4,27 @@
 #include <print>
 
 namespace {
-constexpr auto kMod {100};
+constexpr auto ticks {100ULL};
+std::int64_t pos {50LL};
 };
 
 int main() {
-  std::int64_t position {50LL};
   std::uint64_t zeroes {0};
-  auto floordivision = [](const std::int64_t lhs, const std::int64_t rhs) { return (lhs / rhs) - ((lhs % rhs < 0) ? 1 : 0); };
-  const std::filesystem::path path {"/home/user/Documents/Projects/input.txt"};
-  std::ifstream stream {path};
+  auto floordivision = [](const std::int64_t lhs, const std::int64_t rhs) { return (lhs / rhs) - (lhs % rhs < 0); };
+  std::ifstream stream {std::filesystem::path {"/home/user/Documents/Projects/input.txt"}};
   char side {};
   std::int64_t turn {};
   while (stream >> side >> turn) {
-    std::int64_t old = position; 
+    std::int64_t old = pos; 
     switch (side) {
       case 'L': { 
-        position -= turn;
-        zeroes += floordivision(old - 1, kMod) - floordivision(position - 1, kMod);
+        pos -= turn;
+        zeroes += floordivision(old - 1, ticks) - floordivision(pos - 1, ticks);
         break;
       }
       case 'R': {
-        position += turn;
-        zeroes += floordivision(position, kMod) - floordivision(old, kMod);
+        pos += turn;
+        zeroes += floordivision(pos, ticks) - floordivision(old, ticks);
         break;
       }
       default:
